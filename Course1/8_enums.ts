b30e3335131bb2c106fbeaf911163607818a0e87
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-05-15 14:59:53 +0700

    Enums

---
+++ Course1/8_enums.ts
@@ -0,0 +1,26 @@
+const enum SeatChoice{
+    AISLE ,
+    MIDDLE,
+    WINDOW,
+    FOURTH,
+}
+const enum SeatChoice2{
+    AISLE = 10,
+    MIDDLE= 20,
+    WINDOW,
+    FOURTH,
+}
+
+const enum SeatChoice3{
+    AISLE = "ALISE",
+    MIDDLE = "middle",
+    WINDOW = "window",
+    FOURTH = 12,
+}
+const enum SeatChoice4{
+    AISLE = "aisle",
+    MIDDLE = 3,
+    WINDOW,
+    FOURTH,
+}
+const hcSeat = SeatChoice.AISLE
