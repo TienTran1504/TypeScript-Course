54c86e1dfe5f219a7e423ff41c3ae91a9fe87e8b
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-05-15 18:16:30 +0700

    Instance of and type predicates

--- Course1/Setup/dist/detections.js
+++ Course1/Setup/dist/detections.js
@@ -26,3 +26,29 @@ function printAll(strs) {
         }
     }
 }
+function isAdminAccount(account) {
+    if ("isAdmin" in account) { // check if isAdmin property isAdmin exist in account
+        return account.isAdmin;
+    }
+}
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+function isFish(pet) {
+    return pet.swim !== undefined; // true its a fish
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return "fish food";
+    }
+    else {
+        pet;
+        return "bird food";
+    }
+}
