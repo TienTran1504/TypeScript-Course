5eeb643784f09b08ea61604b2e68bcb207600cf2
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-05-15 14:23:34 +0700

    Arrays

---
+++ Course1/5_arrays.js
@@ -0,0 +1,10 @@
+var heros = [];
+var yourNumber = [];
+var allUser = [];
+var MLModels = [
+    [255, 255, 255],
+    []
+];
+heros.push("spider man");
+yourNumber.push(123);
+allUser.push({ name: "tien", isActive: false });
---
+++ Course1/5_arrays.ts
@@ -0,0 +1,18 @@
+const heros: string[] = [];
+
+const yourNumber: number[] = [];
+
+type User = {
+    name: string,
+    isActive: boolean,
+}
+const allUser: User[] = []; 
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+    []
+];
+
+heros.push("spider man");
+yourNumber.push(123);
+allUser.push({ name: "tien", isActive: false });
