571ebd1efb24cd3a1230a14bdb554fe2fdecd771
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-05-15 13:19:43 +0700

    Variables

---
+++ Course1/2_variables.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "hello";
+var greetings2 = "hi";
+var myNum = 6;
+greetings.toUpperCase;
+myNum.toFixed(2);
+console.log(greetings);
+console.log(greetings2);
+var userId;
+userId = 33455.5;
+var userID = 123123.123;
+userID.toFixed();
+var isLoggedIn = false;
+//primitive variables: boolean, string, number
---
+++ Course1/2_variables.ts
@@ -0,0 +1,21 @@
+let greetings: string = "hello";
+let greetings2= "hi";
+
+let myNum = 6;
+greetings.toUpperCase;
+
+myNum.toFixed(2);
+console.log(greetings);
+console.log(greetings2);
+
+
+let userId: number;
+userId = 33455.5;
+
+let userID: number = 123123.123;
+userID.toFixed();
+
+let isLoggedIn: boolean = false;
+export { } // remove redeclare error
+
+//primitive variables: boolean, string, number
