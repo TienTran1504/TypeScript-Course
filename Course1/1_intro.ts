6c6e775f51f5bceb362fe6d94e52be85ccab777c
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-05-15 13:03:52 +0700

    Intro typescript

---
+++ Course1/1_intro.ts
@@ -0,0 +1,8 @@
+let user = { name: "Tien", age: 21 }
+
+let user2 = {name: "Tien2", age:22}
+console.log("Hello");
+
+console.log(user.age);
+
+// Terminal: tsc 1_intro.ts -> convert new javascript file
