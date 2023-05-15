009d0f58e2db63eecbd82bb83c3e092de4dffe43
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-05-15 21:53:21 +0700

    Todo logic & useRef blur

--- Course2/typescript-tutorial/src/App.tsx
+++ Course2/typescript-tutorial/src/App.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Todo } from './model';
 
 
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>(""); // <string> to define string for useState string | number: union
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos,{id:Date.now(), todo:todo, isDone:false}])
+      setTodo("");
+    }
+  };
+  console.log(todos);
+  
+  // console.log(todo);
+
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField/>
+      <InputField todo={todo} setTodo ={setTodo} handleAdd ={handleAdd} />
+      {/* <TodoList/> */}
+      {todos.map(t => (
+        <li>{t.todo}</li>
+      ))}
     </div>
   );
 }
--- Course2/typescript-tutorial/src/components/InputField.tsx
+++ Course2/typescript-tutorial/src/components/InputField.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./style.css"
-const InputField = () => {
-  return (
-      <form action="" className='input'>
-          <input type="input" placeholder="Enter a task" className="input__box" />
+interface Props{
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
+}
+const InputField: React.FC<Props> = ({todo, setTodo, handleAdd} : Props ) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    return (
+        <form onSubmit={(e) => {
+            handleAdd(e);
+            inputRef.current?.blur(); // ?: it's not sure going to be optional or having any value inside
+
+        }} className='input'>
+            <input
+              ref={inputRef}
+              type="input"
+              placeholder="Enter a task"
+              className="input__box"
+              value={todo}
+              onChange={
+                  (e) => setTodo(e.target.value)
+              }
+          />
           <button className="input_submit" type="submit">
               Go
           </button>
