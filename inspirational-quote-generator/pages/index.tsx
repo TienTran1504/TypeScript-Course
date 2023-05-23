695dfb2fb686126a97e3ee583f05867a3f6f139e
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-05-23 18:33:27 +0700

    Add footer (Quotes Generator)

--- inspirational-quote-generator/pages/index.tsx
+++ inspirational-quote-generator/pages/index.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
-import { GradientBackgroundCon } from "@/components/QuoteGenerator/QuoteGeneratorElements";
 
+//Components
+import {
+  GradientBackgroundCon,
+  BackgroundImage1,
+  BackgroundImage2,
+  FooterCon,
+  FooterLink,
+  RedSpan,
+} from "@/components/QuoteGenerator/QuoteGeneratorElements";
+
+//Assets
+import Clouds1 from "../assets/cloud-and-thunder.png";
+import Clouds2 from "../assets/cloudy-weather.png";
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
   return (
     <>
       <Head>
@@ -13,7 +27,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* Background */}
-      <GradientBackgroundCon></GradientBackgroundCon>
+      <GradientBackgroundCon>
+        <BackgroundImage1 src={Clouds1} height="300" alt="cloudybackground1" />
+        <BackgroundImage2 src={Clouds2} height="300" alt="cloudybackground1" />
+
+        <FooterCon>
+          <>
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Developed with <RedSpan>❤️</RedSpan> by{" "}
+            <FooterLink
+              href="https://www.facebook.com/tien.trandungtien.5/"
+              target="_blank"
+              rel="noopener norefererer">
+              @TienBieber
+            </FooterLink>
+          </>
+        </FooterCon>
+      </GradientBackgroundCon>
     </>
   );
 }
